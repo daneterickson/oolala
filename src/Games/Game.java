a1764754c9afb63a30020cd900fbbed5c41fa600
Norah Tan <xt22@duke.edu>
2021-09-26 20:41:56 -0400

    updated the missing Game

--- src/Games/Game.java
+++ src/Games/Game.java
@@ -6,9 +6,28 @@ import java.util.List;
 
 public class Game {
 
-    private List<Creature> myListCreatures;
+    //    private List<Creature> myListCreatures;
+    private Creature myCurrent;
 
     public Game () {
+        myCurrent = new Creature();
+    }
 
+    public void step (String command) {
+        String[] commands = command.split(" ");
+        myCurrent.setStatusStamp(false);
+        switch (commands[0]) {
+            case "fd" -> myCurrent.move(Integer.valueOf(commands[1]));
+            case "bk" -> myCurrent.move(-Integer.valueOf(commands[1]));
+            case "lt" -> myCurrent.changeOrientation(-Integer.valueOf(commands[1]));
+            case "rt" -> myCurrent.changeOrientation(Integer.valueOf(commands[1]));
+            case "pd" -> myCurrent.setPenActivity(true);
+            case "pu" -> myCurrent.setPenActivity(false);
+            case "st" -> myCurrent.setCreatureVisibility(true);
+            case "ht" -> myCurrent.setCreatureVisibility(false);
+            case "home" -> myCurrent.reset();
+            case "stamp" -> myCurrent.setStatusStamp(true);
+//            case "tell" ->
+        }
     }
 }
