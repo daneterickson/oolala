de14f74dbe2bb5ca896acefc98debc978315ca5b
Dane Erickson <dte12@duke.edu>
2021-09-28 11:24:21 -0400

    updated TurtleView to be turtle Image instead of a rectangle

--- src/View/TurtleView.java
+++ src/View/TurtleView.java
@@ -4,44 +4,52 @@ import Creatures.Creature;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
-public class TurtleView extends GameView{
+public class TurtleView extends GameView {
 
+  public static final String TURTLE_IMAGE = "turtle.png";
   public static final double TURTLE_WIDTH = 10;
   public static final double TURTLE_HEIGHT = 20;
-  public static final Paint SHOW_TURTLE = Color.GREEN;
-  public static final Paint HIDE_TURTLE = Color.TRANSPARENT;
+//  public static final Paint SHOW_TURTLE = Color.GREEN;
+//  public static final Paint HIDE_TURTLE = Color.TRANSPARENT;
 
   private Creature myModel;
-  private Rectangle myTurtle;
+  private Rectangle myTurtleShape;
+  private ImageView myTurtleImage;
 
   public TurtleView(Creature creature) {
-    myTurtle = drawCreature(0,0,TURTLE_WIDTH, TURTLE_HEIGHT);
-    myModel = creature;;
+    drawCreature(0, 0, TURTLE_WIDTH, TURTLE_HEIGHT);
+    myModel = creature;
   }
 
-  protected Rectangle drawCreature (double x, double y, double width, double height) { // final will just be double size for turtle
-    Rectangle tmp = new Rectangle(x, y, width, height);
-    tmp.setFill(SHOW_TURTLE);
-    return tmp;
+  protected void drawCreature(double x, double y, double width,
+      double height) { // final will just be double size for turtle
+    myTurtleImage = new ImageView(new Image(TURTLE_IMAGE));
+    myTurtleImage.setFitHeight(TURTLE_HEIGHT);
+    myTurtleImage.setFitWidth(TURTLE_WIDTH);
+    myTurtleImage.setVisible(true);
   }
 
-  protected Line drawLine () {
-    Line path = new Line(myModel.getOldX(), myModel.getOldY(), myModel.getNewX(), myModel.getNewY());
-    if (myModel.getPenActivity()) return path;
+  protected Line drawLine() {
+    Line path = new Line(myModel.getOldX(), myModel.getOldY(), myModel.getNewX(),
+        myModel.getNewY());
+    if (myModel.getPenActivity()) {
+      return path;
+    }
     return null;
   }
 
-  protected void updateCreature () {
-    myTurtle.setX(myModel.getNewX() + TURTLE_WIDTH/2);
-    myTurtle.setY(myModel.getNewY() + TURTLE_HEIGHT/2);
-    myTurtle.setRotate(myModel.getAngle());
-    if (myModel.getCreatureVisibility()) myTurtle.setFill(HIDE_TURTLE);
-    else myTurtle.setFill(SHOW_TURTLE);
+  protected void updateCreature() {
+    myTurtleImage.setX(myModel.getNewX() + TURTLE_WIDTH / 2);
+    myTurtleImage.setY(myModel.getNewY() + TURTLE_HEIGHT / 2);
+    myTurtleImage.setRotate(myModel.getAngle());
+    myTurtleImage.setVisible(myModel.getCreatureVisibility());
   }
 }
