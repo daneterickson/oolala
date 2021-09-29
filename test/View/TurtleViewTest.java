69381b7573fac1dc36758a763dec8db769afee85
Dane Erickson <dte12@duke.edu>
2021-09-29 16:43:05 -0400

    added View Test folder

---
+++ test/View/TurtleViewTest.java
@@ -0,0 +1,12 @@
+package View;
+
+import javafx.stage.Stage;
+import util.DukeApplicationTest;
+
+public class TurtleViewTest extends DukeApplicationTest {
+
+  @Override
+  public void start (Stage stage) {
+
+  }
+}
