cc36f755122a7f86badede8be33b724f2e265019
Norah Tan <xt22@duke.edu>
2021-09-28 22:08:25 -0400

    implemented turtle tell and can thus adding multiple turtles and added JUnit test for Game.step

--- src/Creatures/Creature.java
+++ src/Creatures/Creature.java
@@ -1,29 +1,14 @@
 package Creatures;
 
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-
 public class Creature {
 
-    private final int WIDTH = 2;
-    private final int HEIGHT = 1;
-    private final Color COLOR_TURTLE = Color.GREEN;
-
-    private boolean isPenActive, isCreatureVisible, isCreatureActive, isStamp;
-
-    // can call myShape.getX(), getY(), getRotate(), myShape.isVisible()
-//    private Rectangle myShape;
-
+    private boolean isPenActive, isCreatureVisible, isStamp;
     private double myOldPosX, myOldPoxY, myNewPosX, myNewPosY;
     private int myAngle;
 
 
     public Creature () {
-//        myShape = new Rectangle(0,0, WIDTH, HEIGHT);
-//        myShape.setFill(COLOR_TURTLE);
-
         isPenActive = true;
-//        isCreatureActive = false;
         isStamp = false;
         isCreatureVisible = true;
 
@@ -35,15 +20,11 @@ public class Creature {
     }
 
     public boolean getPenActivity () { return isPenActive; }
-    public boolean getCreatureVisibility () { return isCreatureVisible; }
     public void setPenActivity (boolean status) { isPenActive = status; }
-    public void setCreatureVisibility (boolean status) {
-//        myShape.setVisible(status);
-        isCreatureVisible = status;
-    }
+    public boolean getCreatureVisibility () { return isCreatureVisible; }
+    public void setCreatureVisibility (boolean status) { isCreatureVisible = status; }
     public boolean getStatusStamp () { return isStamp; }
     public void setStatusStamp (boolean status) { isStamp = status; }
-//    public Rectangle getShape () { return myShape; }
 
     public double getOldX () { return myOldPosX; }
     public double getOldY () { return myOldPoxY; }
@@ -56,9 +37,6 @@ public class Creature {
         myNewPosX = 0;
         myNewPosY = 0;
         myAngle = 0;
-//        myShape.setX(0);
-//        myShape.setY(0);
-//        myShape.setRotate(0);
     }
 
     private void updateOldPos () {
@@ -70,15 +48,10 @@ public class Creature {
         updateOldPos();
         myNewPosX = myOldPosX + distance * Math.cos(Math.toRadians(myAngle));
         myNewPosY = myOldPoxY + distance * Math.sin(Math.toRadians(myAngle));
-//        myShape.setX(myShape.getX() + distance * Math.cos(Math.toRadians(myShape.getRotate())));
-//        myShape.setY(myShape.getY() + distance * Math.sin(Math.toRadians(myShape.getRotate())));
     }
 
     public void changeOrientation (int angle) {
-        // initial getRotate() = 0 -> positive x-axis
-        // positive angle means clockwise
         myAngle += angle;
-//        myShape.setRotate(angle + myShape.getRotate());
     }
 
 }
