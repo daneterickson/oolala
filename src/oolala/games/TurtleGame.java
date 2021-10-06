64466867749e06fbdc5d92859758ca1e41605539
Norah Tan <xt22@duke.edu>
2021-10-06 18:25:54 -0400

    tested expand after refactoring

--- src/oolala/games/TurtleGame.java
+++ src/oolala/games/TurtleGame.java
@@ -1,10 +1,7 @@
 package oolala.games;
 
-import oolala.commands.Command;
 import oolala.creatures.Creature;
 
-import java.util.Arrays;
-import java.util.Iterator;
 
 public class TurtleGame extends Game {
 
@@ -38,86 +35,5 @@ public class TurtleGame extends Game {
             }
         }
     }
-
-    @Override
-    public String compile (String paragraph) {
-        StringBuilder compiledText = new StringBuilder();
-        String[] lines = paragraph.toLowerCase().split("\n");
-
-        for (String command: lines) {
-            String[] commands = command.split(" ");
-            compiledText.append(breakCommands(commands));
-        }
-        return compiledText.toString();
-    }
-
-    private StringBuilder breakCommands (String[] commands) {
-        StringBuilder ret = new StringBuilder();
-        Iterator<String> commandIterator = Arrays.stream(commands).iterator();
-        while (commandIterator.hasNext()) {
-            String temp = commandIterator.next();
-            if (temp.startsWith("#")) break;
-            if (temp.equals("")) continue;
-
-            Command input = new Command(temp);
-            Command result = input.recognize();
-
-            try {
-                if (result.getNumArgs() == 2) {
-                    ret.append(String.format("%s %s\n", temp, commandIterator.next()));
-                }
-                else if (result.getNumArgs() == 1) {
-                    ret.append(String.format("%s\n", temp));
-                }
-                else if (result.getNumArgs() == -1) {
-                    ret.append(String.format("%s ", temp));
-                    while (commandIterator.hasNext()) {
-                        temp = commandIterator.next();
-                        if (isNumeric(temp)) ret.append(String.format("%s ", temp));
-                    }
-                    ret.append("\n");
-                }
-            }
-            catch (Exception e) {
-                System.out.println("Error: Not a valid command");
-                break;
-            }
-        }
-        return ret;
-    }
-
-//            int i = 0;
-//            while (i < commands.length) {
-//                if (commands[i].startsWith("#")) break;
-//                if (commands[i].equals("")) {
-//                    i++;
-//                    continue;
-//                }
-//
-//                Command input = new Command(commands[i]);
-//                Command result = input.recognize();
-//                try {
-//                    if (result.getNumArgs() == 2) {
-//                        ret.append(String.format("%s %s\n", commands[i], commands[i+1]));
-//                        i += result.getNumArgs();
-//                    }
-//                    else if (result.getNumArgs() == 1) {
-//                        ret.append(String.format("%s\n", commands[i]));
-//                        i += result.getNumArgs();
-//                    }
-//                    else if (result.getNumArgs() == -1) {
-//                        ret.append(commands[i] + " ");
-//                        i++;
-//                        while (i < commands.length && isNumeric(commands[i])) {
-//                            ret.append(commands[i] + " ");
-//                            i++;
-//                        }
-//                        ret.append("\n");
-//                    }
-//                }
-//                catch (Exception e) {
-//                    System.out.println("Error: Not a valid command");
-//                }
-//            }
 
 }
