7f7272d26b83b690ccc1f44222bb76a2bbf08051
Norah Tan <xt22@duke.edu>
2021-10-06 16:18:53 -0400

    test

--- src/oolala/games/DarwinGame.java
+++ src/oolala/games/DarwinGame.java
@@ -2,7 +2,10 @@ package oolala.games;
 
 public class DarwinGame extends Game {
 
-    public void step (String command) {}
+    public void step (String command) {
+
+        
+    }
 
     public String compile (String paragraph) {
         return null;
