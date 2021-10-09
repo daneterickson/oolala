aaf775b0eb9e5bbc05626a747bcfc035f6158bd1
Evelyn Cupil-Garcia <ec220@duke.edu>
2021-10-08 20:59:15 -0400

    Added UI components to Darwin/Fractal, need testing and integration

--- src/oolala/view/canvas/DarwinCanvasDisplay.java
+++ src/oolala/view/canvas/DarwinCanvasDisplay.java
@@ -2,6 +2,7 @@ package oolala.view.canvas;
 
 import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -30,7 +31,7 @@ public class DarwinCanvasDisplay extends CanvasDisplay{
     BorderPane panel = new BorderPane();
     panel.setId("CanvasPanel");
     panel.setLeft(makeCanvasPanel());
-    panel.setRight(makeLogoPanel());
+    panel.setRight(makeDarwinPanel());
     return panel;
   }
 
@@ -43,10 +44,51 @@ public class DarwinCanvasDisplay extends CanvasDisplay{
     return pane;
   }
 
-  private Node makeLogoPanel () {
+  private Node makeDarwinPanel () {
     VBox panel = new VBox();
+    panel.setId("DarwinPanel");
+    panel.getChildren().addAll(makeRadiusPanel(), makeSpeciesPanel(), makeDarwinImagePanel());
     return panel;
   }
+
+  private Node makeRadiusPanel() {
+    VBox panel = new VBox();
+    panel.setId("RadiusPanel");
+    Node radius = myDisplayComponents.makeTextBoxWithLabel("RadiusLabel", "RadiusBox", e -> temporary());
+    panel.getChildren().add(radius);
+    return panel;
+  }
+
+  private Node makeSpeciesPanel() {
+    VBox panel = new VBox();
+    panel.setId("SpeciesPanel");
+    Node speciesLabel = myDisplayComponents.makeLabel("SpeciesLabel");
+    Node speciesX = myDisplayComponents.makeTextBoxWithLabel("HomeLocationX", "LocationX", e -> temporary());
+    Node speciesY = myDisplayComponents.makeTextBoxWithLabel("HomeLocationY", "LocationY", e -> temporary());
+    panel.getChildren().addAll(speciesLabel, speciesX, speciesY);
+    return panel;
+  }
+
+  private Node makeDarwinImagePanel() {
+    VBox panel = new VBox();
+    panel.setId("DarwinImagePanel");
+    Node turtleImageLabel = myDisplayComponents.makeLabel("DarwinImageLabel");
+    panel.getChildren().addAll(turtleImageLabel, makeDarwinImagePanelButtons());
+    return panel;
+  }
+
+  private Node makeDarwinImagePanelButtons() {
+    HBox panel = new HBox();
+    panel.setId("DarwinImagePanelButtons");
+    Node catButton = myDisplayComponents.makeButton("CatButton", e -> temporary());
+    Node dogButton = myDisplayComponents.makeButton("DogButton", e -> temporary());
+    Node turtleButton = myDisplayComponents.makeButton("TurtleButton", e -> temporary());
+    panel.getChildren().addAll(catButton, dogButton, turtleButton);
+    return panel;
+  }
+
+  private void temporary() {}
+  
   @Override
   public void updateTurtleStatePanel() {
 
